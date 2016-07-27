6376d82863d0a087e27b0f90189da0cf2065421e
Luan Andrade <luansandrade92@hotmail.com>
2016-07-27 17:12:35 -0300

    hello api

---
+++ contabilizei/src/main/java/com/luan/rest/HelloWorldService.java
@@ -0,0 +1,21 @@
+package com.luan.rest;
+ 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+ 
+@Path("/hello")
+public class HelloWorldService {
+ 
+	@GET
+	@Path("/{param}")
+	public Response getMsg(@PathParam("param") String msg) {
+ 
+		String output = " Olá, API. Seja bem vindo: " + msg;
+ 
+		return Response.status(200).entity(output).build();
+ 
+	}
+ 
+}
